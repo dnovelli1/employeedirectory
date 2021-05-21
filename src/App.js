4e20ee8f8306bc1d5174daae3f62f58de6253412
Jake Novelli <jakenovelli11@gmail.com>
2021-05-20 20:55:54 -0700

    finished styling

--- src/App.js
+++ src/App.js
@@ -31,20 +31,20 @@ function App() {
 
   return (
     <div style={{ textAlign: 'center' }}>
-      <nav className="navbar gradient">
-        <div className="container-fluid">
-          <span className="navbar-brand mb-0 h1">Welcome to your Employee Directory</span>
+      <nav className="navbar" style={{ background: 'grey'}}>
+        <div className="container-fluid" style={{margin: 10}}>
+          <span className="navbar-brand mb-0" style={{fontSize: 25, fontWeight: 'bolder', backgroundColor: 'white', padding: 10, borderRadius: 8}}>Welcome to your Employee Directory</span>
         </div>
         <div className="container">
           <img src="./hero.jpg" className="img-fluid" alt="hero"></img>
         </div>
       </nav>
-      <div className="container-fluid gradient">
-        <label style={{ fontWeight: 'bolder' }}>Search Employee by Name:</label>
-        <input style={{ width: '60%', margin: 10 }} type='text' value={filter} onChange={e => setFilter(e.target.value)}></input>
+      <div className="container-fluid" style={{ background: 'grey'}}>
+        {/* <label style={{ fontWeight: 'bolder' }}>Search Employee by Name:</label> */}
+        <input placeholder='Search Employee by Name' style={{ width: '60%', margin: 10, borderRadius: 5, borderColor: 'transparent'}} type='text' value={filter} onChange={e => setFilter(e.target.value)}></input>
       </div>
         <table className="table">
-          <thead>
+          <thead style={{margin: 10}}>
             <tr>
               <th scope="col">Photo</th>
               <th scope="col">Name</th>
@@ -55,7 +55,7 @@ function App() {
           </thead>
           <tbody>
             {employees?.filter(emp => (emp.name.first + ' ' + emp.name.last).toLowerCase().includes(filter.toLowerCase())).map(employee => (
-              <tr style={{ borderColor: 'lightBlue', borderWidth: 10, margin: 5 }}>
+              <tr style={{ borderColor: 'grey', borderWidth: 10, margin: 5}}>
                 <th scope="row"><img src={employee.picture.medium} alt={employee.name.first} style={{ borderRadius: 5 }}></img></th>
                 <td>{employee.name.first} {employee.name.last}</td>
                 <td>{employee.dob.age}</td>
